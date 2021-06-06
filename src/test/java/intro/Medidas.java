20309b4ae112ff7f4e38dd8717073e6d4ed69b65
franc <frabritto@gmail.com>
2021-06-06 19:10:16 -0300

    Incluí o "Boa noite !!!" no começo da impressão de  texto no console

---
+++ src/test/java/intro/Medidas.java
@@ -0,0 +1,45 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+//3 - Classe
+public class Medidas {
+    //3.1 - Atributos - Características
+
+    //3.2 - Métodos e Funções
+    public static void main(String[] args) {
+        System.out.println("Boa noite !!!:");
+        System.out.println("Cálculo de áreas:");
+
+        //Cálculo de área - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // Largura recebe 4
+        comprimento = 6; //comprimento recebe 3
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura +  " metros e o comprimento de "
+                           + comprimento + " metros a área é de " + resultado+ " m².");
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
